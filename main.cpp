cbb9c8db170463d585ed24b7154e2bb8699e5b95
Vladu_Gabriel-Bogdan <bogdanvladu901@gmail.com>
2024-11-11 21:19:24 +0200

    test commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 int main()
@@ -10,24 +10,37 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
-    inFile >> no_problems;
 
+    inFile >> no_problems;
+    vector<string> name_prob(no_problems);
+    vector<string> speciality1(no_problems);
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> name_prob[i];
+        inFile >> speciality1[i];
     }
 
     inFile >> no_doctors;
-
+    vector<int> numberdoc(no_doctors, 0);
+    vector<string> speciality2(no_doctors);
     for (int i = 0; i < no_doctors; i++)
     {
         inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> speciality2[i];
     }
-
+    for (int i = 0; i < no_problems; i++)
+    {
+        int ok = 0;
+        for (int j = 0; j < no_doctors; j++)
+        {
+            if (speciality1[i] == speciality2[j] && numberdoc[j] == 0)
+            {
+                cout << name_prob[i] << " " << "Acceptat" << endl;
+                numberdoc[j] = 1;
+                ok = 1;
+            }
+        }
+        if (ok == 0) cout << name_prob[i] << " " << "Respins" << endl;
+    }
     return 0;
 }
